a2697d3dbbe13f83c03824e0db1d66027628b256
Lukas Arnold <Lukas.Arnold@student.dhbw-vs.de>
2025-03-07 09:44:23 +0100

    Update auf neueste Version

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pandas as pd
 import requests
 from psycopg2.extras import execute_values
 import json
+import csv
 
 app = Flask(__name__)
 
@@ -42,8 +43,11 @@ def get_stations():
     lon = request.args.get('lon', type=float)
     radius = request.args.get('radius', type=float)
     station_count = request.args.get('stations', type=int)
+    datefrom = request.args.get('datefrom', type=int)
+    dateto = request.args.get('dateto', type=int)
+
     
-    selectedstations = get_stations_within_radius(lat, lon, radius, station_count)
+    selectedstations = get_stations_within_radius(lat, lon, radius, station_count,datefrom,dateto)
     stations = []
     for station_id, station_name,distance, station_lat, station_lon  in selectedstations:
         stations.append({
@@ -72,7 +76,18 @@ def get_station_data():
     dateto = request.args.get('dateto', type=int)
 
     query = """
-        SELECT measure_year, maxyear,minyear,maxspring,minspring,maxsummer,minsummer,maxautumn,minautumn,maxwinter,minwinter FROM stationdata where station_id = %s and measure_year between %s and %s;
+        SELECT measure_year, maxyear, minyear, 
+            CASE WHEN latitude >= 0 THEN maxspring ELSE maxautumn END AS max_spring,
+            CASE WHEN latitude >= 0 THEN minspring ELSE minautumn END AS min_spring,
+            CASE WHEN latitude >= 0 THEN maxsummer ELSE maxwinter END AS max_summer,
+            CASE WHEN latitude >= 0 THEN minsummer ELSE minwinter END AS min_summer,
+            CASE WHEN latitude >= 0 THEN maxautumn ELSE maxspring END AS max_autumn,
+            CASE WHEN latitude >= 0 THEN minautumn ELSE minspring END AS min_autumn,
+            CASE WHEN latitude >= 0 THEN maxwinter ELSE maxsummer END AS max_winter,
+            CASE WHEN latitude >= 0 THEN minwinter ELSE minsummer END AS min_winter
+        FROM stationdata 
+        JOIN station ON stationdata.station_id = station.station_id
+        WHERE stationdata.station_id = %s AND measure_year BETWEEN %s AND %s and measure_year between measure_from and measure_to;
     """
 
     with DatabaseConnection() as cursor:
@@ -80,8 +95,12 @@ def get_station_data():
         fetcheddata = cursor.fetchall()
         cursor.connection.commit()
         
+    if not fetcheddata:
+        return jsonify({"error":f"Von der Station {stationid} wurden im Jahresbereich von {datefrom} bis {dateto} keine Daten gefunden."}),404
+
     df = pd.DataFrame(fetcheddata, columns=["year","max","min","springmax","springmin","summermax","summermin","autumnmax","autumnmin","wintermax","wintermin"])
-    # Struktur für Charts
+    df = df.where(pd.notna(df), 0)
+    # Struktur für Charts 
     data = {
         "years": df["year"].tolist(),
         "seasons": {
@@ -98,6 +117,7 @@ def get_station_data():
     yearlytemplate = render_template("yearlytabledata.html",data=seasontabledata, stationid = stationid)
     return jsonify(data=data, seasontemplate = seasontemplate,yearlytemplate = yearlytemplate)
 
+### Anfang Kopie in data.py
 
 def create_tables():
     with DatabaseConnection() as cursor:
@@ -107,6 +127,8 @@ def create_tables():
                 "latitude" NUMERIC(7,4) NOT NULL,
                 "longitude" NUMERIC(7,4) NOT NULL,
                 "station_name" character(100) NOT NULL,
+                "measure_from" smallint NOT NULL,
+                "measure_to" smallint NOT NULL,
                 "station_point" geography(Point, 4326) NOT NULL
             );''')
             cursor.execute('''CREATE TABLE IF NOT EXISTS stationdata (
@@ -128,68 +150,100 @@ def create_tables():
         except Exception as ex:
             print(ex)
 
-import csv
 
-def get_ghcn_stations(file_path):
+
+#müsste das nicht insert heißen?
+def get_ghcn_stations(csv_file_path, txt_file_path):
     with DatabaseConnection() as cursor:
         try:
             cursor.execute("SELECT EXISTS(SELECT 1 FROM station)")
             exists = cursor.fetchone()[0]
-            if not exists:
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    reader = csv.reader(file) 
-                    next(reader)
+            if exists:
+                print("Datenbank ist bereits befüllt.")
+                return  
 
-                    for row in reader:
-                        if len(row) < 4:
-                            continue
-                        
-                        station_id = row[0].strip()
-                        latitude = float(row[1].strip())
-                        longitude = float(row[2].strip())
-                        station_name = row[5].strip()
+            #only valid years stations from inventory
+            df_inventory = pd.read_csv(txt_file_path, sep=r'\s+', header=None, 
+                names=["station_id", "latitude","longitude","measure_type","measure_from_year","measure_to_year"])
+            df_inventory = df_inventory[df_inventory["measure_type"].isin(["TMIN", "TMAX"])].copy()  
 
-                        cursor.execute('''
-                            INSERT INTO station (station_id, latitude, longitude, station_name, station_point)
-                            VALUES (%s, %s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s)::geography, 4326))
-                            ON CONFLICT (station_id) DO NOTHING;
-                        ''', (station_id, latitude, longitude, station_name, longitude, latitude))
+            df_pivot = df_inventory.pivot(index=["station_id", "latitude", "longitude"], 
+                                          columns="measure_type", 
+                                          values=["measure_from_year", "measure_to_year"])
+            df_pivot.columns = ["measure_from_TMAX", "measure_from_TMIN", "measure_to_TMAX", "measure_to_TMIN"]
+            df_pivot = df_pivot.reset_index()
+
+            df_pivot = df_pivot.dropna()
+            df_pivot["measure_from"] = df_pivot[["measure_from_TMAX", "measure_from_TMIN"]].max(axis=1)
+            df_pivot["measure_to"] = df_pivot[["measure_to_TMAX", "measure_to_TMIN"]].min(axis=1)
+            df_inventory = df_pivot[["station_id", "latitude", "longitude", "measure_from", "measure_to"]]
+            df_inventory.loc[:, "measure_from"] = df_inventory["measure_from"].astype(int)
+            df_inventory.loc[:, "measure_to"] = df_inventory["measure_to"].astype(int)
+
+            #read csv to get the names of the stations
+            df_stations = pd.read_csv(csv_file_path, header=None, usecols=[0, 4, 5], names=["station_id", "station_state","station_name"])
+            #only get the station names that are in the inventory
+            df_stations["station_with_state"] = df_stations["station_state"].fillna("") + " " + df_stations["station_name"]
+            station_name_dict = df_stations.set_index("station_id")["station_with_state"].to_dict()
+
+            df_inventory.loc[:, "station_name"] = df_inventory["station_id"].map(station_name_dict)
+            df_inventory.loc[:, "station_name"] = df_inventory["station_name"].str.strip()
+
+            insert_query = """
+                INSERT INTO station (station_id, latitude, longitude, station_name, measure_from, measure_to, station_point)
+                VALUES (%s, %s, %s, %s,%s,%s, ST_SetSRID(ST_MakePoint(%s, %s), 4326))
+                ON CONFLICT (station_id) DO NOTHING;
+            """
+            for _, row in df_inventory.iterrows():
+                cursor.execute(insert_query, (row["station_id"], row["latitude"], row["longitude"], row["station_name"], row["measure_from"], row["measure_to"], row["longitude"], row["latitude"]))
+
             cursor.connection.commit()
         except Exception as ex:
             print(f"Fehler beim Einfügen: {ex}")
 
 
 def insert_ghcn_by_year(year):
-    gzipped_file = download_file(year)
-    if gzipped_file:
+    gzipped_file_this_yr = download_file(year)
+    gzipped_file_last_yr = download_file(str(int(year)-1))
+    list_winter = [1, 2]
+    if gzipped_file_this_yr:
         with DatabaseConnection() as cursor:
             try:
-                with gzip.open(gzipped_file, "rt", encoding="utf-8") as f:
-                    df = pd.read_csv(f, delimiter=",", header=None, usecols=[0, 1, 2, 3],
-                                     names=["station_id", "measure_date", "measure_type", "measure_value"])
-                    df = df[df["measure_type"].isin(["TMAX", "TMIN"])]
-                    df["measure_date"] = pd.to_datetime(df["measure_date"].astype(str), format='%Y%m%d')
-                    df["measure_year"] = df["measure_date"].dt.year
+                with gzip.open(gzipped_file_this_yr, "rt", encoding="utf-8") as f1:
+                    df_this_yr = prepare_dataframe(f1)
+                    #Prüfen ob Vorjahr vorhanden
+                    if gzipped_file_last_yr:
+                        list_winter.append(12)
+                        with gzip.open(gzipped_file_last_yr, "rt", encoding="utf-8") as f2:
+                            df_last_yr = prepare_dataframe(f2)
+                            december_last_yr = df_last_yr[df_last_yr["measure_date"].dt.month == 12]
+                            # Delete values of december of the dataframe of this year
+                            df_this_yr = df_this_yr[~(df_this_yr["measure_date"].dt.month == 12)]
+                            # Insert values of december last year to the dataframe of this year
+                            df_this_yr = pd.concat([df_this_yr, december_last_yr]).sort_values(by=['station_id', 'measure_type', 'measure_date'])
+                            df_this_yr.reset_index(drop=True, inplace=True)
                     # Erstellung der Jahreszeiten zur weiteren Berechnung der Mins und Maxs
-                    df["season"] = df["measure_date"].dt.month.apply(lambda month: 'Winter' if month in [12, 1, 2]
+                    df_this_yr["season"] = df_this_yr["measure_date"].dt.month.apply(lambda month: 'Winter' if month in list_winter
                                                                      else ('Spring' if month in [3, 4, 5]
                                                                            else ('Summer' if month in [6, 7, 8] 
-                                                                                 else 'Autumn')))
+                                                                                 else ('Autumn' if month in [9, 10, 11] else 'Unknown'))))
                     #Umrechnung in Dezimalzahl
-                    df["measure_value"] = df["measure_value"] / 10 
-                    yearly_data = df.groupby(["station_id", "measure_year"]).agg(
-                        maxyear=('measure_value', 'max'),
-                        minyear=('measure_value', 'min'),
-                        maxspring=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Spring'].max() if 'Spring' in df.loc[x.index, 'season'].values else None),
-                        minspring=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Spring'].min() if 'Spring' in df.loc[x.index, 'season'].values else None),
-                        maxsummer=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Summer'].max() if 'Summer' in df.loc[x.index, 'season'].values else None),
-                        minsummer=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Summer'].min() if 'Summer' in df.loc[x.index, 'season'].values else None),
-                        maxautumn=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Autumn'].max() if 'Autumn' in df.loc[x.index, 'season'].values else None),
-                        minautumn=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Autumn'].min() if 'Autumn' in df.loc[x.index, 'season'].values else None),
-                        maxwinter=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Winter'].max() if 'Winter' in df.loc[x.index, 'season'].values else None),
-                        minwinter=('measure_value', lambda x: x[df.loc[x.index, 'season'] == 'Winter'].min() if 'Winter' in df.loc[x.index, 'season'].values else None)
+                    df_this_yr["measure_value"] = df_this_yr["measure_value"] / 10 
+                    df_this_yr["measure_year"] = year
+                    yearly_data = df_this_yr.groupby(["station_id", "measure_year"]).agg(
+                        # Annual average for TMAX and TMIN
+                        max_year=('measure_value', lambda x: round(x[df_this_yr.loc[x.index, 'measure_type'] == 'TMAX'].mean(), 1)),
+                        min_year=('measure_value', lambda x: round(x[df_this_yr.loc[x.index, 'measure_type'] == 'TMIN'].mean(), 1)),
+                        # Seasonal averages for TMAX and TMIN
+                        maxspring=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Spring') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMAX')].mean(), 1) if 'Spring' in df_this_yr.loc[x.index, 'season'].values else None),
+                        minspring=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Spring') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMIN')].mean(), 1)  if 'Spring' in df_this_yr.loc[x.index, 'season'].values else None),
+                        maxsummer=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Summer') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMAX')].mean(), 1) if 'Summer' in df_this_yr.loc[x.index, 'season'].values else None),
+                        minsummer=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Summer') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMIN')].mean(), 1) if 'Summer' in df_this_yr.loc[x.index, 'season'].values else None),
+                        maxautumn=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Autumn') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMAX')].mean(), 1) if 'Autumn' in df_this_yr.loc[x.index, 'season'].values else None),
+                        minautumn=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Autumn') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMIN')].mean(), 1) if 'Autumn' in df_this_yr.loc[x.index, 'season'].values else None),
+                        maxwinter=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Winter') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMAX')].mean(), 1) if 'Winter' in df_this_yr.loc[x.index, 'season'].values else None),
+                        minwinter=('measure_value', lambda x: round(x[(df_this_yr.loc[x.index, 'season'] == 'Winter') & (df_this_yr.loc[x.index, 'measure_type'] == 'TMIN')].mean(), 1) if 'Winter' in df_this_yr.loc[x.index, 'season'].values else None)
                     ).reset_index()
-
                     data_to_insert = list(yearly_data.itertuples(index=False, name=None))
                     insert_query = """
                         INSERT INTO stationdata (station_id, measure_year, maxyear, minyear,maxspring,minspring, 
@@ -204,7 +258,6 @@ def insert_ghcn_by_year(year):
             except Exception as ex:
                 print(f"Fehler beim Verarbeiten der Datei: {ex}")
 
-
 def download_file(year):
     baseURLbyYear = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/PLACEHOLDER.csv.gz"
     print(f'Herunterladen der {year} Datei.')
@@ -216,8 +269,18 @@ def download_file(year):
     print("Herunterladen abgeschlossen")
     return io.BytesIO(response.content)
 
-def fill_database():
-    for year in range(2000,2024):
+def prepare_dataframe(file):
+    df = pd.read_csv(file, delimiter=",", header=None, usecols=[0, 1, 2, 3],
+                        names=["station_id", "measure_date", "measure_type", "measure_value"])
+    df = df[df["measure_type"].isin(["TMAX", "TMIN"])]
+    df["measure_date"] = pd.to_datetime(df["measure_date"].astype(str), format='%Y%m%d')
+    df["measure_year"] = df["measure_date"].dt.year    
+    return df
+
+
+def fill_database(start, end):
+    #TODO: prüfen warum Range nicht funktioniert hat
+    for year in range(start, end):
         with DatabaseConnection() as cursor:
             try:
                 cursor.execute("SELECT EXISTS(SELECT 1 FROM stationdata where measure_year = %s)",[year])
@@ -229,11 +292,15 @@ def fill_database():
                 print(f"Fehler beim Verarbeiten der Datei: {ex}")
 
 #create_tables()
-#fill_database()
 #insert_ghcn_by_year("2024")
-#get_ghcn_stations("./data/ghcnd-stations.csv")
+#get_ghcn_stations("./data/ghcnd-stations.csv","./data/ghcnd-inventory.txt")
+#fill_database(1750, 2024)
+
+#### Ende Kopie in data.py
+
 
 # Benötigt, damit PostGIS aktiviert ist (nur einmalig, danach kommt sonst ein Fehler)
+# Kann das in die Funktion?
 with DatabaseConnection() as cursor:
     cursor.execute("SELECT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'postgis');")
     postgis_installed = cursor.fetchone()[0]
@@ -247,7 +314,7 @@ if not postgis_installed:
 else:
     print("PostGIS is already enabled.")
 
-def get_stations_within_radius(lat_ref, lon_ref, radius, number):
+def get_stations_within_radius(lat_ref, lon_ref, radius, number,year_from,year_to):
     """
     Retrieves stations and their distance to a reference point within a given radius.
 
@@ -270,12 +337,12 @@ def get_stations_within_radius(lat_ref, lon_ref, radius, number):
         ELSE station_name 
     END AS station_name, ROUND(CAST(ST_Distance(station_point, ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography) / 1000 AS NUMERIC), 2) AS distance, latitude, longitude
     FROM station
-    WHERE ST_DWithin(station_point, ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography, %s * 1000)
+    WHERE ST_DWithin(station_point, ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography, %s * 1000) AND measure_from between %s and %s
     ORDER BY distance
     LIMIT %s;
     """
     with DatabaseConnection() as cursor:
-        cursor.execute(query, (lat_ref, lon_ref, lon_ref, lat_ref, lon_ref, lat_ref, radius, number))
+        cursor.execute(query, (lat_ref, lon_ref, lon_ref, lat_ref, lon_ref, lat_ref, radius, number,year_from,year_to))
         stations = cursor.fetchall()
         cursor.connection.commit()
     stations = [(station_id, station_name, float(distance), latitude, longitude) for station_id, station_name, distance, latitude, longitude in stations] # Convert distance from Decimal (needed for ROUND) to float.
