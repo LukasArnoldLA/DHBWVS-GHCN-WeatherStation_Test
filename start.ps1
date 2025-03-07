18a1f5a7cb5f32f26cf3493a9e7f6f1a26c4ee09
Lukas Arnold <Lukas.Arnold@student.dhbw-vs.de>
2025-03-07 09:49:48 +0100

    Anpassung Skript

--- start.ps1
+++ start.ps1
@@ -1,5 +1,5 @@
 Write-Host "docker-compose.yml wird heruntergeladen."
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Praciqx/DHBWVS-GHCN-WeatherStation/main/docker-compose.yml" -OutFile ".\docker-compose.yml"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/LukasArnoldLA/DHBWVS-GHCN-WeatherStation_Test/main/docker-compose.yml" -OutFile ".\docker-compose.yml"
 Write-Host "Images werden heruntergeladen und Container erstellt."
 docker compose up -d
 Write-Host "Container sind erstellt und fahren nun hoch, Website http://localhost:5000 wird in 5 Sekunden aufgerufen."
